47c9826d789c9093934363d037fdcee3eb75a194
Daniel Diaz <ddiaz006@ucr.edu>
2023-03-28 14:26:55 -0500

    first commit of condor scripts

---
+++ condor/runJobs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export PATH=${PATH}:/cvmfs/cms.cern.ch/common
+export CMS_PATH=/cvmfs/cms.cern.ch
+
+export SCRAM_ARCH=slc7_amd64_gcc630
+scramv1 project CMSSW CMSSW_9_4_4
+cd CMSSW_9_4_4/src
+scramv1 build -j 10
+eval `scramv1 runtime -sh` # cmsenv
+cd ../../
+
+hostname
+echo make an ls
+ls
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+echo " "
+echo pwd and ls
+echo " "
+pwd
+ls
+
+mkdir roots
+
+
+./RunAnalyzer.exe --Path=$1 --Sample=$2
+
+mv roots/*.root .
+ls
---
+++ condor/subJobs.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+doSubmit=false
+path="root://cmsxrootd.fnal.gov//store/user/ddiaz/B-Parking/V1p19_0"
+
+samples=(  \
+"BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi0p3_ctau300" \
+"ParkingBPH4_2018A" \
+)
+
+#compile analyzer
+pushd ../analysis/
+make clean; make 
+popd
+echo ""
+read -p $'\e[1;31m Please Provide a version name for these jobs: \e[0m' vName
+
+mkdir -p gitignore/$vName
+mkdir -p gitignore/$vName/logs
+
+cd gitignore/$vName
+setUp="./setup.C"
+grep -h "loggit" ../../../analysis/app/* >> ${setUp}
+
+# write base for submit file
+printf "universe = vanilla\n" > submitfile
+printf "Executable = ../../runJobs.sh\n" >> submitfile
+printf "Should_Transfer_Files = YES \n" >> submitfile
+printf "WhenToTransferOutput = ON_EXIT\n" >> submitfile
+printf "Transfer_Input_Files = ../../../analysis/RunAnalyzer.exe\n" >> submitfile
+
+printf "notify_user = $(whoami)@cern.ch\n" >> submitfile
+printf "x509userproxy = $X509_USER_PROXY\n" >> submitfile
+printf "\n" >> submitfile
+printf "Output = logs/runanalyzer_\$(Cluster)_\$(Process).stdout\n" >> submitfile
+printf "Error  = logs/runanalyzer_\$(Cluster)_\$(Process).stderr\n" >> submitfile
+printf "Log    = logs/runanalyzer_\$(Cluster)_\$(Process).log\n" >> submitfile
+printf "\n" >> submitfile
+
+
+for sample in ${samples[@]}
+do
+  printf "Arguments = $path/$sample/ $sample \n" >> submitfile
+  printf "Queue\n" >> submitfile
+  printf "\n" >> submitfile
+done
+
+if [ ${doSubmit} = true ]
+then
+ condor_submit submitfile
+fi
+
+cd ../..
