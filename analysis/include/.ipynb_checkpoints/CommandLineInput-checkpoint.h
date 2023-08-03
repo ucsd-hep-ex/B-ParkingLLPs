75909d2ca511300dc03c95a6ea9db26c46034af3
Anthony Aportela <aaportel@ucsd.edu>
2023-08-03 09:08:05 -0500

    Remove all tracked .ipynb_checkpoints directories

--- analysis/include/.ipynb_checkpoints/CommandLineInput-checkpoint.h
+++
@@ -1,9 +0,0 @@
-#ifndef CommandLineInput_H
-#define CommandLineInput_H
-// C++ INCLUDES
-#include <iostream>
-#include <string>
-
-std::string ParseCommandLine(int argc, char *argv[], std::string opt);
-
-#endif
