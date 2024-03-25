340295a779d13d1f7c948fefaab957641d507657
arhayrap <aram.hayrapetyan19@gmail.com>
2024-03-25 13:47:21 -0500

    acceptance

--- acceptance/main.C
+++ acceptance/main.C
@@ -1,9 +1,10 @@
 #include "Acceptance.h"
 #include <iostream>
 
-int main(int argc, char* argv[]){
-
-  TString Sample=argv[1]; // "/uscms/home/ahayrape/nobackup/BToPhiK_Analysis/New_signal/BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi0p3_ctau300.root";
+int main(int argc, char * argv[]){
+  // "/eos/uscms/store/user/ahayrape/BToLLPK_signal_samples/" + *
+  // char * base = "/eos/uscms/store/user/ahayrape/BToLLPK_signal_samples/";
+  TString Sample= argv[1]; // "/uscms/home/ahayrape/nobackup/BToPhiK_Analysis/New_signal/BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi0p3_ctau300.root";
   std::cout<<Sample<<std::endl;
   TChain* chain = new TChain("MuonSystem");
   chain->Add(Sample);
