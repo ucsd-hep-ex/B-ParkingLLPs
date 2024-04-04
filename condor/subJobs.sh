acff974d6ff6f5e868ca33f9e8cdba04874e8ebd
Daniel Diaz <ddiaz006@ucr.edu>
2024-04-04 11:09:49 -0500

    One script for signal and data condor jobs

--- condor/subJobs.sh
+++ condor/subJobs.sh
@@ -6,36 +6,38 @@ path="root://cmsxrootd.fnal.gov/"
 #path="root://cmsxrootd.fnal.gov//store/user/aaportel/B-Parking/V1p19_7"
 
 lists=(  \
-"ParkingBPH1_2018A" \
-"ParkingBPH2_2018A" \
-"ParkingBPH3_2018A" \
-"ParkingBPH4_2018A" \
-"ParkingBPH5_2018A" \
-"ParkingBPH6_2018A" \
-"ParkingBPH1_2018B" \
-"ParkingBPH2_2018B" \
-"ParkingBPH3_2018B" \
-"ParkingBPH4_2018B" \
-"ParkingBPH5_2018B" \
-"ParkingBPH6_2018B" \
-"ParkingBPH1_2018C" \
-"ParkingBPH2_2018C" \
-"ParkingBPH3_2018C" \
-"ParkingBPH4_2018C" \
-"ParkingBPH5_2018C" \
-"ParkingBPH1_2018D" \
-"ParkingBPH2_2018D" \
-"ParkingBPH3_2018D" \
-"ParkingBPH4_2018D" \
-"ParkingBPH5_2018D" \
+"ParkingBPH_test" \
+"BToKPhi_MuonLLPDecayGenFilter_PhiToPi0Pi0_mPhi0p3_ctau300" \
+)
+#"ParkingBPH2_2018A" \
+#"ParkingBPH3_2018A" \
+#"ParkingBPH4_2018A" \
+#"ParkingBPH5_2018A" \
+#"ParkingBPH6_2018A" \
+#"ParkingBPH1_2018B" \
+#"ParkingBPH2_2018B" \
+#"ParkingBPH3_2018B" \
+#"ParkingBPH4_2018B" \
+#"ParkingBPH5_2018B" \
+#"ParkingBPH6_2018B" \
+#"ParkingBPH1_2018C" \
+#"ParkingBPH2_2018C" \
+#"ParkingBPH3_2018C" \
+#"ParkingBPH4_2018C" \
+#"ParkingBPH5_2018C" \
+#"ParkingBPH1_2018D" \
+#"ParkingBPH2_2018D" \
+#"ParkingBPH3_2018D" \
+#"ParkingBPH4_2018D" \
+#"ParkingBPH5_2018D" \
+
+ctau_values=(1\ 
+               3    5    7    10 \
+               30   50   70   100 \
+               300  500  700  1000 \
+               3000 5000 7000 10000 \
 )
 
-#ctau_values=(1\ 
-#               3    5    7    10 \
-#               30   50   70   100 \
-#               300  500  700  1000 \
-#               3000 5000 7000 10000)
-#
 #compile analyzer
 pushd ../analysis/
 make clean; make 
@@ -50,11 +52,18 @@ cp ../../../analysis/RunAnalyzer.exe .
 setUp="./setup.C"
 grep -h "loggit" ../../../analysis/app/* >> ${setUp}
 
-for listName in ${lists[@]}
-do
-  mkdir -p $listName
-  mkdir -p $listName/logs
-  cd $listName 
+
+prep_and_submit(){
+  local listName=$1
+  local ctau=${2:-}
+  Name=$listName
+  if [ -n "$ctau" ]; then
+    Name=${listName}_to_${ctau}
+  fi
+  
+  mkdir -p $Name
+  mkdir -p $Name/logs
+  cd $Name 
   # write base for submit file
   printf "universe = vanilla\n" > submitfile
   printf "Executable = ${CMSSW_BASE}/src/B-ParkingLLPs/condor/runJobs.sh\n" >> submitfile
@@ -70,7 +79,11 @@ do
   printf "Log    = logs/runanalyzer_\$(Cluster)_\$(Process).log\n" >> submitfile
   printf "\n" >> submitfile
   samples_file=${listdir}/${listName}.list
-  echo $samples_file
+  if [ -n "$ctau" ]; then
+    echo $samples_file to $ctau
+  else
+    echo $samples_file
+  fi
   if [ -e "$samples_file" ]; then
     # Read sample entries from the file and store them in an array
     mapfile -t samples < "$samples_file"
@@ -91,7 +104,11 @@ do
       sampleName_="${sample##*/}"
       thePath="${path}${path_}"
     fi 
-    printf "Arguments = $thePath $sampleName_ \n" >> submitfile
+    if [ -n "$ctau" ]; then 
+      printf "Arguments = $thePath $sampleName_ $ctau\n" >> submitfile
+    else
+      printf "Arguments = $thePath $sampleName_ "" \n" >> submitfile
+    fi
     printf "Queue\n" >> submitfile
     printf "\n" >> submitfile
   done
@@ -101,5 +118,18 @@ do
     condor_submit submitfile
   fi
   cd ..
+}
+
+##----------------------------------------Main
+for listName in ${lists[@]}
+do
+  if [[ $listName == *"Parking"* ]]; then
+    prep_and_submit "$listName"
+  else
+    for ctau in ${ctau_values[@]}
+    do
+      prep_and_submit "$listName" "$ctau"
+    done
+  fi
 done
 cd ../..
