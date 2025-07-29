b82a52f8a35c7f2f8d9030af16dffbf8acfc8ea8
Daniel Diaz <ddiaz006@ucr.edu>
2025-07-28 19:05:42 -0500

    code for FR plots

---
+++ plotters/FR/README.md
@@ -0,0 +1,3 @@
+This is the code that made the was used to make the plots for the final reading version of the paper and so-forth
+
+root files: /eos/user/d/ddiaz/B-ParkingLLPs/rootsForFR
---
+++ plotters/FR/runPlotterSimple.sh
@@ -0,0 +1,10 @@
+root -l -b -q 'Plotter_AN.C("nominal", 'false', "/uscms/home/ahayrape/nobackup/BToPhiK_Analysis/B-ParkingLLPs/HEP_Data_Entries/CutflowTables/", "1D_histograms_pi0pi0")'
+root -l -b -q 'Plotter_AN.C("nominal", 'true', "/uscms/home/ahayrape/nobackup/BToPhiK_Analysis/B-ParkingLLPs/HEP_Data_Entries/CutflowTables/", "1D_histograms_pi0pi0")'
+
+rm /uscms/home/ddiaz/nobackup/BParkingLLPs/CMSSW_14_0_6_patch1/src/B-ParkingLLPs/plotters/FR/plotDump/cscRechitClusterDPhiLeadMuon_nominal.pdf
+rm /uscms/home/ddiaz/nobackup/BParkingLLPs/CMSSW_14_0_6_patch1/src/B-ParkingLLPs/plotters/FR/plotDump/cscRechitClusterSize_nominal.pdf
+rm /uscms/home/ddiaz/nobackup/BParkingLLPs/CMSSW_14_0_6_patch1/src/B-ParkingLLPs/plotters/FR/plotDump/cscRechitClusterTime_nominal_log.pdf
+
+rm /uscms/home/ddiaz/nobackup/BParkingLLPs/CMSSW_14_0_6_patch1/src/B-ParkingLLPs/plotters/FR/plotDump/cscRechitClusterDPhiLeadMuon_nominal.C
+rm /uscms/home/ddiaz/nobackup/BParkingLLPs/CMSSW_14_0_6_patch1/src/B-ParkingLLPs/plotters/FR/plotDump/cscRechitClusterSize_nominal.C
+rm /uscms/home/ddiaz/nobackup/BParkingLLPs/CMSSW_14_0_6_patch1/src/B-ParkingLLPs/plotters/FR/plotDump/cscRechitClusterTime_nominal_log.C
